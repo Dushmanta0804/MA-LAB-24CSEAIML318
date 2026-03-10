234d5d33eb95d401e3574e77b81d87514f28e649
Dushmanta0804 <222288289+Dushmanta0804@users.noreply.github.com>
2026-03-10 16:03:49 +0530

    Create EXP1(Q1).py

---
+++ EXP1(Q1).py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 4, 5, 4, 5])
+
+m = np.sum((x - np.mean(x)) * (y - np.mean(y))) / np.sum((x - np.mean(x))**2)
+b = np.mean(y) - m * np.mean(x)
+
+print("Slope coefficient:", m)
+print("Intercept:", b)
+y_pred = m * x + b
+plt.scatter(x, y, color='blue', label='Data Points')
+plt.plot(x, y_pred, color='red', label='best fitted Line')
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Linear Regression ")
+plt.legend()
+plt.show()
+ 
